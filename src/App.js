5d48dd432a3b1402d57bcd0d213d1c35852e88fb
Gilson96 <grafael99@gmail.com>
2024-09-25 16:49:00 +0100

    Mobile version still in production

--- src/App.js
+++ src/App.js
@@ -5,17 +5,23 @@ import MovieDetails from './components/Movies/MovieDetails';
 import SerieDetails from './components/Series/SerieDetails'
 import MyMovies from './components/Account/MyMovies';
 import MySeries from './components/Account/MySeries'
+import useScreenSize from './features/useScreenSize';
 
 function App() {
+  const screenSize = useScreenSize()
   return (
     <div className={`flex flex-col w-full h-full bg-background bg-no-repeat bg-cover bg-center`}>
-      <Routes>
-        <Route path='/' element={<Dashboard />}/>
+      {screenSize.width < 760 ?
+        <p>Mobile version still in production</p>
+        :
+        <Routes>
+          <Route path='/' element={<Dashboard />} />
           <Route path='/movies/:movieId' element={<MovieDetails />} />
           <Route path='/movies/myMovies' element={<MyMovies />} />
           <Route path='/series/mySeries' element={<MySeries />} />
           <Route path='/series/:serieId' element={<SerieDetails />} />
-      </Routes>
+        </Routes>
+      }
     </div>
   );
 }
