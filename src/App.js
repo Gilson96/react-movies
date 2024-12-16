4d4406923015937ee407e7a8d49657be32957b4e
Gilson96 <grafael99@gmail.com>
2024-12-16 16:50:15 +0000

    Fixed Layout, added mobile views, refactor, improved files organization

--- src/App.js
+++ src/App.js
@@ -3,11 +3,9 @@ import Dashboard from './components/Dashboard'
 import { Route, Routes } from 'react-router-dom'
 import MovieDetails from './components/Movies/MovieDetails';
 import MyMovies from './components/Account/MyMovies';
-import useScreenSize from './features/useScreenSize';
 import AllMovies from './components/Movies/AllMovies';
 
 function App() {
-  const screenSize = useScreenSize()
   return (
     <div className={`flex flex-col w-full h-full bg-[#090E17] bg-no-repeat bg-cover bg-center`}>
    
